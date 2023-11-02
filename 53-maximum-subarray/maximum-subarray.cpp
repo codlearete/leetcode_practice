4097363261a629f894c214c6ac032164e1e8e17a
Jir Et Katharpi <114835911+codlearete@users.noreply.github.com>
2023-11-02 12:51:57 +0530

    Time: 89 ms (31.98%) | Memory: 68.3 MB (12.08%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,20 +3,47 @@ public:
     int maxSubArray(vector<int>& nums) {
 
         int n=nums.size();
-        int max_ending_here=0;
-        int max_so_far=INT_MIN;
+        int maxi=INT_MIN;
+        int sum=0;
 
         for(int i=0;i<n;i++){
-            max_ending_here += nums[i];
-            if(max_ending_here > max_so_far){
-                max_so_far = max_ending_here;
-            }
-            if(max_ending_here < 0){
-                max_ending_here = 0;
-            }
-        }
+            sum += nums[i];
 
-        return max_so_far;
+            if(maxi<sum){
+                maxi=sum;
+            }
+
+            if(sum<0){
+                sum = 0;
+            }
+
+            // maxi = max(maxi,sum);
+        }
+        return maxi;
+
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum += nums[j];
+        //         maxi = max(maxi,sum);
+        //     }
+            
+        // }
+        return maxi;
+        // int max_ending_here=0;
+        // int max_so_far=INT_MIN;
+
+        // for(int i=0;i<n;i++){
+        //     max_ending_here += nums[i];
+        //     if(max_ending_here > max_so_far){
+        //         max_so_far = max_ending_here;
+        //     }
+        //     if(max_ending_here < 0){
+        //         max_ending_here = 0;
+        //     }
+        // }
+
+        // return max_so_far;
         
     }
 };
