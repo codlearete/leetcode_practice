8d0174841b13729b112730bc569a2cae2482106a
Jir Et Katharpi <114835911+codlearete@users.noreply.github.com>
2023-11-02 20:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (60.97%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,31 +4,22 @@ public:
 
         int n=nums.size();
 
-        vector<int>ans(n);
+        int count0=0,count1=0,count2=0;
 
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0){
-                ans[j++]=0;
-
-            }
-        }
-        int k=j;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 1){
-                ans[k++]=1;
-
-            }
-        }
-        int l=k;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 2){
-                ans[l++]=2;
-
-            }
-        }
         for(int i=0;i<n;i++){
-            nums[i]=ans[i];
+            if(nums[i] == 0){
+                count0++;
+            }
+            if(nums[i] == 1){
+                count1++;
+            }
+            if(nums[i] == 2){
+                count2++;
+            }
         }
+
+        for(int i=0;i<count0;i++)nums[i]=0;
+        for(int i=count0;i<count0+count1;i++)nums[i]=1;
+        for(int i=count0+count1;i<n;i++)nums[i]=2;
     }
 };
