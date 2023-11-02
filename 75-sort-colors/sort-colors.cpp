2af632def78e432d8cc07a2b77c1041dc8e23a03
Jir Et Katharpi <114835911+codlearete@users.noreply.github.com>
2023-11-02 21:27:03 +0530

    Time: 4 ms (33.21%) | Memory: 8.5 MB (60.97%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,22 +4,23 @@ public:
 
         int n=nums.size();
 
-        int count0=0,count1=0,count2=0;
+        int low=0;
+        int mid=0;
+        int high=n-1;
 
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0){
-                count0++;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
             }
-            if(nums[i] == 1){
-                count1++;
+            else if(nums[mid] == 1){
+                mid++;
             }
-            if(nums[i] == 2){
-                count2++;
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
-
-        for(int i=0;i<count0;i++)nums[i]=0;
-        for(int i=count0;i<count0+count1;i++)nums[i]=1;
-        for(int i=count0+count1;i<n;i++)nums[i]=2;
     }
 };
