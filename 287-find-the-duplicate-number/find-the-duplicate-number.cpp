0d31c4e950bd67d04e698d336a486cbba4670d37
Jir Et Katharpi <114835911+codlearete@users.noreply.github.com>
2023-11-04 17:16:55 +0530

    Time: 79 ms (75.82%) | Memory: 63.8 MB (24.83%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n, 0); // Fixed the vector initialization
+        int ans = 0;
 
-        sort(nums.begin(),nums.end());
-        int ans;
-
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                ans= nums[i];
+        for (int i = 0; i < n; i++) { // Initialize i to 0
+            if (freq[nums[i]] == 0) {
+                freq[nums[i]] += 1;
+            } else {
+                ans = nums[i];
             }
         }
         return ans;
-        
     }
 };
